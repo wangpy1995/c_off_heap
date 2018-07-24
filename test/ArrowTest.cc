7900635b1505ea160376db6c975ce59e286cf12b
wangpengyu6 <wangpy1995@gmail.com>
2018-07-24 20:30:17 +0800

    test

--- test/ArrowTest.cc
+++ test/ArrowTest.cc
@@ -2,15 +2,34 @@
 // Created by wpy on 18-7-22.
 //
 
+#include <iostream>
 #include "ArrowTest.h"
 
 #include "arrow/io/api.h"
 #include "arrow/ipc/api.h"
 #include "arrow/api.h"
 
-int main() {
-    arrow::MemoryPool *pool = arrow::default_memory_pool();
+static arrow::MemoryPool *pool = arrow::default_memory_pool();
 
+void TestReader(arrow::Table &table) {
+    arrow::TableBatchReader tableBatchReader(table);
+    int64_t num_rows = table.num_rows();
+    int i, j;
+    std::shared_ptr<arrow::RecordBatch> recordBatch;
+    auto ids = std::static_pointer_cast<arrow::Int8Array>(table.column(0)->data()->chunk(0));
+    auto comments = std::static_pointer_cast<arrow::BinaryArray>(table.column(1)->data()->chunk(0));
+    auto comments_values = comments->value_data();
+    const uint8_t *comments_ptr = comments_values->data() + comments->offset();
+    for (i = 0; i < num_rows; ++i) {
+        auto id = ids->Value(i);
+        auto comment_first = comments->value_offset(i);
+        auto comment_last = comments->value_offset(i + 1);
+        std::vector<char> comment(comment_first, comment_last);
+    }
+
+}
+
+int main() {
     arrow::Int8Builder int8Builder;
     int i;
     for (i = 1; i <= 9; ++i) {
@@ -24,7 +43,7 @@ int main() {
     int j;
     for (i = 1; i <= 9; ++i) {
         for (j = 0; j < 6; ++j) {
-            str[j] = static_cast<char>(random());
+            str[j] = static_cast<char>(64 + i);
         }
         binaryBuilder.Append(str);
     }
@@ -46,6 +65,8 @@ int main() {
     arrow::MakeTable(schema, {id_array, comment_array}, &table);
     writer.get()->WriteTable(*(table.get()));
     writer.get()->Close();
+
+    TestReader(*(table.get()));
 
     return 0;
 
